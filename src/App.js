65c96a9cd81ac65c6cbc5122ae954c219fdfa459
igor <embiggenerd@gmail.com>
2019-12-20 11:44:37 -0500

    router set up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
 import Header from "./components/Header.js";
+import { Switch, Route } from 'react-router-dom'
+import WelcomePage from './components/WelcomePage'
 
 
 export default function App() {
   return (
     <main>
-      <Header />
+      <Switch>
+
+        <Route path="/">
+          <Header />
+          <WelcomePage />
+        </Route>
+
+      </Switch>
+
+
     </main>
   );
 }
